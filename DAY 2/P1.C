9fcd1f394136bb39b4a0d202f921b59d9c9759b5
ASHRITHA KUKKESHREE <153017442+Ashrithakukkeshree@users.noreply.github.com>
2025-03-07 06:45:01 +0000

    average

---
+++ DAY 2/P1.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int average =0;
+    puts ("enter a number to chcek sqaure:");
+    scanf("%d",&average);
+    if (average<0&&average<=49)
+    puts("result is fail");
+    else if (average>=50&&average<=79)
+    puts("result is good");
+    else if (average>=80&&average<=89)
+    puts("result is above");
+    else 
+    puts("distintion");
+}
